20149da6c9fba1fd6384350443c76ee469cb7554
Aldo Serrano <aldoantonio1723@gmail.com>
2026-03-23 10:56:14 -0600

    feat: Creando el proyecto

---
+++ to-do-list_backend/src/test/java/com/aldoproyectos/todolist_backend/ToDoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldoproyectos.todolist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
